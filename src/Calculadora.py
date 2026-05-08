35cd482d8548c8e074278ce7dbe32e7d377e5977
gabrielquispe27-ops <gabriel.quispe.27@unsch.edu.pe>
2026-05-08 15:58:21 -0500

    trabajo realizado

--- src/Calculadora.py
+++ src/Calculadora.py
@@ -2,10 +2,10 @@ def suma(a, b):
     return a + b
 
 def resta(a, b):
-    return a - b
+    return a + b
 
 def multiplicacion(a, b):
-    return a * b
+    return a + b
 
 def division(a, b):
     return a / b
